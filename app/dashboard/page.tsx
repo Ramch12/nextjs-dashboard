3b6e7712bda7f84aa92ed2b94e6e57acb8d9850f
Ram Ashish <aram97010@gmail.com>
2024-05-01 12:00:35 +0530

    added changes to dashboard

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,14 @@
+const page = () => {
+  return (
+    <div>
+      <h1 className="border-2 border-solid border-black text-center text-2xl text-red-500">
+        You have requested for dashboard Page component
+      </h1>
+      <div className="mx-auto mt-10 h-20 w-1/2 border-2 border-solid border-black py-8">
+        <p>I think your request has been boosted you confidence too much</p>
+      </div>
+    </div>
+  );
+};
+
+export default page;
